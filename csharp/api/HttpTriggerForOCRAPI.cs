014ad65abae3beaaf4c09fb4b95b8762db4ed111
yamachu <yamachu.dev@gmail.com>
2020-11-14 13:59:16 +0900

    OCR APIも分離

--- csharp/api/HttpTriggerForOCRAPI.cs
+++ csharp/api/HttpTriggerForOCRAPI.cs
@@ -7,15 +7,14 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Net.Http;
-using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
 using CVVisualizer.API.Extension;
+using CVVisualizer.Core;
 
 namespace CVVisualizer.API
 {
     public static class HttpTriggerForReadOCR
     {
-        readonly static string uriBase = "vision/v3.0/ocr";
         readonly static HttpClient client = new HttpClient();
 
         [FunctionName("HttpTriggerForOCRAPI")]
@@ -28,29 +27,19 @@ namespace CVVisualizer.API
                 return new BadRequestObjectResult(exc.Message);
             }
             
-            // ref: https://docs.microsoft.com/ja-jp/azure/cognitive-services/computer-vision/quickstarts/csharp-print-text#create-and-run-the-sample-application
-            var requestParameters = "language=ja&detectOrientation=true";
-            var request = new HttpRequestMessage(HttpMethod.Post, $"{reqParam.Value.Endpoint}{uriBase}?${requestParameters}");
-            request.Headers.Add("ContentType", "application/json");
-            request.Headers.Add("Ocp-Apim-Subscription-Key", reqParam.Value.SubscriptionKey.ToString());
-            
             using var binaryReader = new BinaryReader(reqParam.Value.Image.OpenReadStream());
             var imageByteData = binaryReader.ReadBytes((int)reqParam.Value.Image.Length);
-            using var content = new ByteArrayContent(imageByteData);
-            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
-            request.Content = content;
 
             try {
-                var response = await client.SendAsync(request);
-                var contentString = await response.Content.ReadAsStringAsync();
-                var jsonContent = JToken.Parse(contentString);
+                var result = await VisionOCRService.
+                    AnalyzeAsync(client, reqParam.Value.Endpoint, reqParam.Value.SubscriptionKey, imageByteData)
+                    .ConfigureAwait(false);
+                
+                var jsonContent = JToken.Parse(result);
 
-                if (response.IsSuccessStatusCode)
-                    return new JsonResult(jsonContent);
-                else 
-                    return new BadRequestObjectResult(contentString);
-            } catch (Exception) {
-                return new BadRequestObjectResult("Unexpected error");
+                return new JsonResult(jsonContent);
+            } catch (Exception e) {
+                return new BadRequestObjectResult(e.Message);
             }
         }
     }
---
+++ csharp/core/VisionOCRService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CVVisualizer.Core
+{
+    public static class VisionOCRService
+    {
+        readonly static string uriBase = "vision/v3.1/ocr";
+
+        public static async Task<string> AnalyzeAsync(HttpClient client, string endpoint, string subscriptionKey, byte[] image)
+        {
+            // ref: https://docs.microsoft.com/ja-jp/azure/cognitive-services/computer-vision/quickstarts/csharp-print-text#create-and-run-the-sample-application
+            var requestParameters = "language=ja&detectOrientation=true";
+            var request = new HttpRequestMessage(HttpMethod.Post, $"{endpoint}{uriBase}?${requestParameters}");
+            request.Headers.Add("ContentType", "application/json");
+            request.Headers.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+            
+            using var content = new ByteArrayContent(image);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            request.Content = content;
+
+            var response = await client.SendAsync(request);
+            var contentString = await response.Content.ReadAsStringAsync();
+            if (response.IsSuccessStatusCode) {
+                throw new ArgumentException(contentString);
+            }
+            
+            return contentString;
+        }
+    }
+}
