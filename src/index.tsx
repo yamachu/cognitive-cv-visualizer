1b056bbc2a90b15865dedb3dfb648c4326c01252
yamachu <yamachu.dev@gmail.com>
2020-11-21 04:05:00 +0900

    いい感じにinterface切ってwasmとapiを切り替えられるようにしておく

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,19 @@
 import React from "react";
 import { render } from "react-dom";
+import { createVisionAPIExecutor } from "./api";
 import { App } from "./App";
 
-render(<App />, document.querySelector("#root"));
+const bootstrap = () => {
+  const HOST = process.env.API_HOST as string;
+  const is_blazor = process.env.IS_BLAZOR;
+
+  const executor = createVisionAPIExecutor(HOST);
+
+  return is_blazor
+    ? window.Blazor.start().then((_) => executor)
+    : Promise.resolve(executor);
+};
+
+bootstrap().then((executor) =>
+  render(<App callApi={executor} />, document.querySelector("#root"))
+);
