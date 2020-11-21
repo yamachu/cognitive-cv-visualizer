a50833857959438219598256c9fdd1d08926328a
yamachu <yamachu.dev@gmail.com>
2020-11-21 09:50:09 +0900

    wasmとおしゃべりするところの実装した

--- src/wasm.ts
+++ src/wasm.ts
@@ -8,6 +8,53 @@ export const createVisionAPIWasmExecutor = (): VisionAPIExecutor => (
   cbThen: (json: any) => void,
   cbCatch: (e: any) => void
 ) => {
-  // Todo: impl
-  return Promise.resolve();
+  const formEndpoint = formData.get("endpoint") as string;
+  const formSubscriptionKey = formData.get("subscriptionKey") as string;
+  const formFile = formData.get("file") as File;
+  const toBase64Promise = formFile.arrayBuffer().then((v) => {
+    const arr = new Uint8Array(v);
+    return window.btoa(
+      arr.reduce((prev, curr) => {
+        const next = String.fromCharCode(curr);
+        return prev + next;
+      }, "")
+    );
+  });
+
+  const call = (base64File: string) => {
+    switch (endpoint) {
+      case Endpoint.OCR:
+        return window.DotNet.invokeMethodAsync(
+          "CVVisualizer.Blazor",
+          "RunOCR",
+          formEndpoint,
+          formSubscriptionKey,
+          base64File
+        );
+      case Endpoint.Read:
+        return window.DotNet.invokeMethodAsync(
+          "CVVisualizer.Blazor",
+          "RunRead",
+          formEndpoint,
+          formSubscriptionKey,
+          base64File
+        );
+    }
+  };
+
+  return toBase64Promise
+    .then(call)
+    .then((resp) => {
+      const maybeJsonString = resp;
+      if (typeof maybeJsonString !== "string") {
+        throw new Error(`response is not desired format, ${maybeJsonString}`);
+      }
+      try {
+        return JSON.parse(maybeJsonString);
+      } catch (e) {
+        throw e;
+      }
+    })
+    .then(cbThen)
+    .catch(cbCatch);
 };
